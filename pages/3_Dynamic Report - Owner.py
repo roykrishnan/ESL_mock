32bf02e5e366ee856a1105454479974c8b590c33
Roy Krishnan <60521440+roykrishnan@users.noreply.github.com>
2025-09-03 23:34:12 -0400

    Update 3_Dynamic Report - Owner.py

--- pages/3_Dynamic Report - Owner.py
+++ pages/3_Dynamic Report - Owner.py
@@ -194,7 +194,7 @@ def main_smart_dashboard():
     try:
         st.sidebar.image("logo.png", width=200)
     except:
-        st.sidebar.image("../images/logo.png", width=200)
+        pass
     
     st.sidebar.title("How This Works")
     st.sidebar.write("**Smart Learning System**")
